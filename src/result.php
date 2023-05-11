67f325a2f33f49187aef6b7cc49ef05a4fbd06b7
Joseph <jmtjcb@gmail.com>
2023-05-11 11:37:15 -0600

    Asthetics

--- src/result.php
+++ src/result.php
@@ -40,44 +40,46 @@
     </form>
 
     <?php
-        // $servername = "localhost";
-        // $username = "student";
-        // $password = "CompSci364";
-        // $dbname = "databased";
+        $servername = "localhost";
+        $username = "student";
+        $password = "CompSci364";
+        $dbname = "databased";
 
-        // // Create connection
-        // $conn = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-        // // Check connection
-        // if ($conn->connect_error) {
-        //     die("Connection failed: " . $conn->connect_error);
-        // }else{
-        //     //echo "Successful connection";
-        // }
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }else{
+            //echo "Successful connection";
+        }
 
 
-        // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        //     $myInputValue = $_POST['searchbox'];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $myInputValue = $_POST['searchbox'];
 
-        //     // do something with $myInputValue
-        //     $stmt = $conn->prepare("SELECT * FROM Posts WHERE instructor LIKE ?");
+            // do something with $myInputValue
+            $stmt = $conn->prepare("SELECT * FROM Posts WHERE instructor LIKE ?");
 
-        //     $instructor = "%$myInputValue%";
-        //     $stmt->bind_param("s", $instructor);
-        //     $stmt->execute();
+            $instructor = "%$myInputValue%";
+            $stmt->bind_param("s", $instructor);
+            $stmt->execute();
 
-        //     $result = $stmt->get_result();
-        //     while ($row = $result->fetch_assoc()) {
-        //         echo "<div class=\"postBox\"><h4>Anonymous&nbsp".$row["instructor"]."</h4><br><p>".$row["content"]."</p></div>";
-        //     }
-        //     $stmt->close();
-        //     $result->free();
+            echo "<div class = \"spacing\">";
+            $result = $stmt->get_result();
+            while ($row = $result->fetch_assoc()) {
+                echo "<div class=\"postBox\"><h4>Anonymous<br>".$row["instructor"]."</h4><br><p>".$row["content"]."</p></div>";
+            }
+            echo "</div>";
+            $stmt->close();
+            $result->free();
 
-        //     $conn->close();
-        // }
+            $conn->close();
+        }
     ?>
 
-    <div class = "spacing">
+    <!-- <div class = "spacing">
         <div class="postBox">
             <h4>Anonymous<br>Joel Coffman</h4>
             <br><p>insert review here</p>
@@ -90,6 +92,6 @@
             <h4>Anonymous<br>Joel Coffman</h4>
             <br><p>insert review here 3</p>
         </div>
-    </div>
+    </div> -->
 
 </html> 
